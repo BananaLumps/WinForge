f029c186b697b07e3194e1e2925a98a8ca605ab0
Michael Donnelly <michaeld1992@hotmail.com>
2025-06-25 17:30:29 +1200

    Created dependency service

---
+++ WinForge.Common/DependencyService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Concurrent;
+
+namespace WinForge.Common
+{
+    public class DependencyService : IDependencyService
+    {
+        private readonly ConcurrentDictionary<Type, object> _services = new();
+
+        /// <summary> Registers a dependency instance of type T. </summary>
+        internal void Register<T>(T instance) where T : class
+        {
+            var type = typeof(T);
+            if (_services.ContainsKey(type))
+                return;
+
+            _services[type] = instance;
+        }
+
+        /// <inheritdoc />
+        public T GetDependency<T>() where T : class, new()
+        {
+            var type = typeof(T);
+
+            if (_services.TryGetValue(type, out var instance))
+                return (T)instance;
+
+            var created = new T();
+            _services[type] = created;
+            return created;
+        }
+
+        /// <inheritdoc />
+        public bool TryGetDependency<T>(out T? instance) where T : class
+        {
+            var type = typeof(T);
+
+            if (_services.TryGetValue(type, out var existing))
+            {
+                instance = (T)existing;
+                return true;
+            }
+
+            instance = null;
+            return false;
+        }
+    }
+}
